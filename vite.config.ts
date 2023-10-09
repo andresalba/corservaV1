d42a80d82cedcc29c4ff81a5bb07ef2281a3d787
Andres <tensinkusum@hotmail.com>
2023-10-09 10:59:06 -0500

    carpeta corservaV1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://andresalba.github.io/corservaV1/'
+  //base: 'https://andresalba.github.io/corservaV1/'
 })
